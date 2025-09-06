53082955613633e3e6d0be64ad79488b6073614e
Student-ChinmayaMohanty <mohantychinmaya199@gmail.com>
2025-09-06 20:45:30 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./assets/index.css",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
